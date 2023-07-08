aad4edcbcda4d97c4f6de57ab6c74eeda5686a5c
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-08 12:39:50 +0530

    added winston and mongo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import app from './app.js';
-import dotenv from 'dotenv';
+import app from "./app.js";
+import logger from "./configs/logger.config.js";
+import mongoose from "mongoose";
 
-dotenv.config();
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res)=>{
-    res.send('You got something?')
+mongoose.connection.on("error",(err) => {
+    logger.error(`Mongodb connection error: ${err}`);
+    process.exit (1);
 });
 
-app.post('/test', (req, res)=>{
-    res.send('Finally got something!')
+if(process.env.NODE_ENV !== "production"){
+    mongoose.set("debug", true);
+}
+
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to Mongodb");
+  });
+
+let server = app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed");
+    process.exit(1);
+  }
 });
