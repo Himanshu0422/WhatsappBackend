44bf985ac810ab9abbd6bd56055dce9bcd6849c0
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-10 14:10:45 +0530

    routes

--- src/index.js
+++ src/index.js
@@ -5,47 +5,46 @@ import mongoose from "mongoose";
 const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
 
-mongoose.connection.on("error",(err) => {
-    logger.error(`Mongodb connection error: ${err}`);
-    process.exit (1);
+mongoose.connection.on("error", (err) => {
+	logger.error(`Mongodb connection error: ${err}`);
+	process.exit(1);
 });
 
-if(process.env.NODE_ENV !== "production"){
-    mongoose.set("debug", true);
+if (process.env.NODE_ENV !== "production") {
+	mongoose.set("debug", true);
 }
 
 mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    logger.info("Connected to Mongodb");
-  });
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+}).then(() => {
+		logger.info("Connected to Mongodb");
+});
 
 let server = app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
+	logger.info(`Server is running on port ${PORT}`);
 });
 
 const exitHandler = () => {
-  if (server) {
-    logger.info("Server closed");
-    process.exit(1);
-  } else {
-    process.exit(1);
-  }
+	if (server) {
+		logger.info("Server closed");
+		process.exit(1);
+	} else {
+		process.exit(1);
+	}
 };
 
 const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
+	logger.error(error);
+	exitHandler();
 };
 
 process.on("uncaughtException", unexpectedErrorHandler);
 process.on("unhandledRejection", unexpectedErrorHandler);
 
 process.on("SIGTERM", () => {
-  if (server) {
-    logger.info("Server closed");
-    process.exit(1);
-  }
+	if (server) {
+		logger.info("Server closed");
+		process.exit(1);
+	}
 });
