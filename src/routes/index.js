c6e4566b138ddffdc988cabc27a91d63e7b7f386
Himanshu0422 <himanshumittal035@gmail.com>
2023-07-14 23:21:37 +0530

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import authRoutes from './auth.route.js';
+import conversationRoutes from './coversation.route.js';
+import messageRoutes from './message.route.js';
 
 const router = express.Router();
 router.use('/auth', authRoutes);
+router.use('/conversation', conversationRoutes);
+router.use('/message', messageRoutes);
 
 export default router;
